0b0b927e9e1048d15bc65ae29bcaf73be4067834
fxsloker <sloker.new@gmail.com>
2014-09-26 01:04:59 +0400

    commit9

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
 require "/vendor/autoload.php";
+
+// create new Slim instance
+$app = new \Slim\Slim();
+ 
+// add new Route
+$app->get('/', function() use ($app) {
+ 	echo "<h1>Hello Slim World</h1>";
+});
+ 
+// run the Slim app
+$app->run();
